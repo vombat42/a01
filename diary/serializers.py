b6a9211693e2134bd8474c7ed3258e4d3d392e30
andre <aaa.newwork@gmail.com>
2023-04-26 00:06:45 +0300

    change disign in Diary v.01

--- diary/serializers.py
+++ diary/serializers.py
@@ -20,6 +20,9 @@ class EntrySerializer(ModelSerializer):
 	chapter_name = serializers.CharField(source='chapter.name')
 	user_name = serializers.CharField(source='user.username')
 	links = LinkSerializer(read_only=True, many=True)
+	date_start = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
+	date_end = serializers.DateTimeField(format="%d-%m-%Y %H:%M:%S")
+
 	class Meta:
 		model = Entry
 		fields = ['id','chapter_name', 'date_start', 'date_end','text', 'user_name', 'links', 'grade']
